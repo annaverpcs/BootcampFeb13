ac751c37947b5c44778452c56d6586981d15ccf7
Daniil Boussel <daniil7142@gmail.com>
2024-03-25 10:57:50 -0400

    Scenario FEB-267 DONE

--- src/test/java/definitions/DanBStepDefs.java
+++ src/test/java/definitions/DanBStepDefs.java
@@ -1,7 +1,11 @@
 package definitions;
 
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static support.TestContext.getDriver;
 
 
@@ -11,6 +15,104 @@ public class DanBStepDefs {
         getDriver().get(appName);
     }
 
+//_____________________Click buttons
+    @Then("Dan click {string} button")
+    public void danClickButton(String ButtonName) {
+
+        switch (ButtonName) {
+
+            case "Login":
+                getDriver().findElement(By.xpath("//button[contains(text(),'Login')]")).click();
+                break;
+            case "Sign In":
+                getDriver().findElement(By.xpath("//button[contains(text(),'Sign in')]")).click();
+                break;
+            case "Add specialist":
+                getDriver().findElement(By.xpath("//button[contains(text(),'Add specialist')]")).click();
+                break;
+            case "Specialist":
+                getDriver().findElement(By.xpath("//option[@value='doctor']")).click();
+                break;
+            case "Monday":
+                getDriver().findElement(By.xpath("//button[@id='monday']")).click();
+                break;
+            case "Save":
+                getDriver().findElement(By.xpath("//button[contains(text(),'Save')]")).click();
+                break;
+            case "Delete":
+                String deleteSpecialist = "//td[text()='FirstnameSelenium LastNameSelenium']/following-sibling::td/div/span/span[contains(text(),'delete')]";
+                getDriver().findElement(By.xpath(deleteSpecialist)).click();
+                break;
+            case "Remove specialist":
+                getDriver().findElement(By.xpath("//button[contains(text(),'Remove specialist')]")).click();
+                break;
+            default:
+                System.out.println("Button '" + ButtonName + "' not found");
+
+        }
+    }
+
+
+
+//__________________Field input
+    @Then("I type {string} to {string} field")
+    public void iTypeToField(String value, String TextFieldName) {
+String fieldXpath="";
+
+        switch (TextFieldName) {
+            case "email" -> fieldXpath="//input[@id='email']";
+            case "password" ->fieldXpath = "//input[@id='password']";
+            case "First name" -> fieldXpath = "//input[@id='first_name']";
+            case "Last name" -> fieldXpath = "//input[@id='last_name']";
+            case "working_hours.monday.from" -> fieldXpath = "//input[@id='working_hours.monday.from']";
+            case "working_hours.monday.to" -> fieldXpath = "//input[@id='working_hours.monday.to']";
+            default -> {System.out.println("Field " + TextFieldName + " not found");}
+
+        };
+        getDriver().findElement(By.xpath(fieldXpath)).sendKeys(value);
+    }
+
+
+
+//_____________________assertion isTrue
+    @And("Dan confirming  text {string} displayed on page")
+    public void danConfirmingTextDisplayedOnPage(String text) {
+String actualXpath = "";
+      String addnewSpecialist =  "//span[text()='New specialist']";
+              String PatientAccountName = "//h1[contains(text(),'Ben Simpson')]";
+              String adminAccountName = "//h1[contains(text(),'Calvin Grant')]";
+              String NewSpesialist ="//td[contains(text(),'FirstnameSelenium LastNameSelenium')]";
+              String popUpremovalConfirm = "//p[contains(text(),'Are you sure you want to remove this specialist?')]";
+
+        switch (text) {
+
+            case "New specialist" -> actualXpath = addnewSpecialist;
+            case "Ben Simpson" -> actualXpath = PatientAccountName;
+            case "Calvin Grant" -> actualXpath = adminAccountName;
+            case "FirstnameSelenium LastNameSelenium" ->actualXpath=NewSpesialist;
+            case "Are you sure you want to remove this specialist?"->actualXpath = popUpremovalConfirm;
+            default -> {System.out.println("Confirmation text '"+ text + "' not found");}
+        };
+        assertThat(getDriver().findElement(By.xpath(actualXpath)).isDisplayed()).isTrue();
+    }
+
+    //________________________Assertion isFalse
+    @Then("Dan confirming  text {string} is not displayed on page")
+    public void danConfirmingTextIsNotDisplayedOnPage(String text) {
+        //assertThat(getDriver().findElement(By.xpath("//td[contains(text(),'"+text+"')]")).isDisplayed()).isFalse();
+
+        assertThat(getDriver().findElements(By.xpath("//td[contains(text(),'"+text+"')]")).isEmpty());
+    }
+
+    @And("Dan refresh current page")
+    public void danRefreshCurrentPage() {
+        getDriver().navigate().refresh();
+    }
+
+    @And("wait for {int} sec")
+    public void waitForSec(int sec) throws Exception {
+        Thread.sleep(sec * 1000);
+    }
 
 
 
--- src/test/resources/features/DanSeleniumTest.feature
+++ src/test/resources/features/DanSeleniumTest.feature
@@ -1,15 +1,44 @@
 @medicenter
-  Feature: Medicenter Testing Scenarios Selenium WebDriver methods
-Scenario: LogIn Selenium WebDriver methods
+Feature: Medicenter Testing Scenarios Selenium WebDriver methods
+
+  Background:
     When Dan open url  "https://medicenter-qa2.vercel.app/"
+    Then Dan click "Login" button
+
+  Scenario: LogIn Selenium WebDriver methods
+
+    Then I type "patient2@gmail.com" to "email" field
+    Then I type "abc123" to "password" field
+    Then Dan click "Sign In" button
+    Then I wait for 3 sec
+    And Dan confirming  text "Ben Simpson" displayed on page
 
 
+#FEB-267
+  Scenario: As an administrator I can add new specialist
+    Then I type "administrator2@gmail.com" to "email" field
+    Then I type "abc123" to "password" field
+    Then Dan click "Sign In" button
+    And wait for 2 sec
+    And Dan confirming  text "Calvin Grant" displayed on page
+    Then Dan click "Add specialist" button
+    And wait for 1 sec
+    And Dan confirming  text "New specialist" displayed on page
+    Then I type "FirstnameSelenium" to "First name" field
+    Then I type "LastNameSelenium" to "Last name" field
+      # pick specialist from list
+    Then Dan click "Specialist" button
+    Then Dan click "Monday" button
+    Then I type "0800" to "working_hours.monday.from" field
+    Then I type "1600" to "working_hours.monday.to" field
+    Then Dan click "Save" button
+    And wait for 1 sec
+    And Dan refresh current page
+    And wait for 2 sec
+    And Dan confirming  text "FirstnameSelenium LastNameSelenium" displayed on page
+    And Dan click "Delete" button
+    And Dan confirming  text "Are you sure you want to remove this specialist?" displayed on page
+    And Dan click "Remove specialist" button
+    And wait for 2 sec
+    Then Dan confirming  text "FirstnameSelenium LastNameSelenium" is not displayed on page
 
-    Scenario: Login as patient
-      Given I open url "https://medicenter-qa2.vercel.app/"
-      Then I click on element with xpath "//button[contains(text(),'Login')]"
-    #sign_in page
-      Then I wait for element with xpath "//label[contains(text(),'Email address')]" to be present
-      Then I type "patient2@gmail.com" into element with xpath "//input[@id='email']"
-      Then I type "abc123" into element with xpath "//input[@id='password']"
-      And I click on element with xpath "//button[contains(text(),'Sign in')]"
